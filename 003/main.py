54dd50445c2d5a4df5bba3b558c624df4754a016
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-02 16:25:38 +0330

    update: 003 structure

--- 003/main.py
+++ 003/main.py
@@ -7,27 +7,27 @@ degree = lambda x, y : math.atan2(y, x)
 factor = lambda r, d : r * (d**2)
 
 
-# input 
-sets = [
-  (1,1),(10,-1),(64,1),(5,0),(12,2),
-  (-1,-1),(6,-6),(12,11),(100,-5),(50,20),
-]
+def convert_to_polar(sets: list) -> list:
+  # create our factors
+  array = []
+  for item in sets:
+    x, y = item
+    r = radius(x, y)
+    d = degree(x, y)
+    d = d if d != 0 else 1
+    array.append({
+      "set": item,
+      "factor": factor(r, d),
+    })
+    
+  return array
 
-# create our factors
-array = []
-for item in sets:
-  x, y = item
-  r = radius(x, y)
-  
-  d = degree(x, y)
-  if d == 0:
-    d = 1
-  
-  f = factor(r, d)
+if __name__ == "__main__":
+  # input coordinates
+  sets = [
+    (1,1),(10,-1),(64,1),(5,0),(12,2),
+    (-1,-1),(6,-6),(12,11),(100,-5),(50,20),
+  ]
 
-  array.append({
-    "set": item,
-    "factor": f,
-  })
-
-print(array)
+  # convert to polar
+  p = convert_to_polar(sets)
