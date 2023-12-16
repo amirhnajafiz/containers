474eaa7f10fee0ffc5d5fdadd29d4966d2887fcb
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-16 09:50:02 +0330

    add: requests for repos

--- src/js/api/requests.js
+++ src/js/api/requests.js
@@ -1,3 +1,36 @@
 // address: https://api.github.com/users/amirhnajafiz-learning/repos
-// sub address: https://raw.githubusercontent.com/amirhnajafiz-learning/ansible/main/README.md
 // helper func: https://stackoverflow.com/questions/35442329/visualizing-readme-md-files-in-my-website
+
+async function repositories() {
+    return (username) => {
+        fetch(`https://api.github.com/users/${username}/repos`)
+            .then((response) => response.json())
+            .then((data) => {
+                list = [];
+
+                data.forEach(el => {
+                    list.push({
+                        "id": el['id'],
+                        "name": el['name'],
+                        "description": el['description']
+                    });
+                });
+
+                return list;
+            })
+            .catch((e) => {
+                console.log(e);
+
+                return [];
+            })
+    }
+}
+
+async function readme() {
+
+}
+
+export const api = {
+    pull: repositories(),
+    read: readme()
+}
