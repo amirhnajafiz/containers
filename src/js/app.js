cac4dcb36881e07c1646b0694c3464916c6f53fd
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-16 10:01:48 +0330

    add: app logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
 // load plugins
+import editor from "./plugins/markdown.js";
+import { api } from "./api/requests.js";
 
 // load components
+import getSlideComponent from "./components/slide.js";
+
+// variables
+const user = "amirhnajafiz-learning";
+
+// logic
+const repos = await api.pull(user);
+const wrapper = document.createElement("div");
+
+repos.forEach(repo => {
+    let txt = api.read(user, repo['name'], repo['branch']);
+    let slide = editor(getSlideComponent(), txt);
+
+    wrapper.appendChild(slide);
+});
 
 // mount component to main app
-document.getElementById("app").innerHTML = undefined;
+document.getElementById("app").innerHTML = wrapper;
--- src/js/components/slide.js
+++ src/js/components/slide.js
@@ -1,1 +1,8 @@
 // https://www.w3schools.com/howto/howto_js_slideshow.asp
+function getSlideComponent() {
+    let el = document.createElement('pre');
+
+    return el;
+}
+
+export default getSlideComponent;
