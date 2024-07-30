cf83413f65248c9826c8dbad7b9ee624bbbf2d57
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-30 17:11:48 +0330

    add: enable cgroups in cgroup.go

--- internal/cgroup/cgroup.go
+++ internal/cgroup/cgroup.go
@@ -1,8 +1,19 @@
 package cgroup
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+	"strconv"
 
-func EnableCgroup(path string) {
-	cgroups := path // /root/mygrp
+	"github.com/amirhnajafiz-learning/containers/pkg/conditions"
+	"github.com/amirhnajafiz-learning/containers/pkg/enums"
+)
+
+// cgroups := /root/mygrp
+// memory := 2M
+func EnableCgroup(cgroups string, memory string) {
 	pids := filepath.Join(cgroups, "child")
+
+	conditions.Must(os.WriteFile(filepath.Join(pids, "memory.max"), []byte(memory), enums.PermGroupAll))
+	conditions.Must(os.WriteFile(filepath.Join(pids, "cgroup.proc"), []byte(strconv.Itoa(os.Getpid())), enums.PermGroupAll))
 }
---
+++ pkg/enums/permissions.go
@@ -0,0 +1,5 @@
+package enums
+
+const (
+	PermGroupAll = 0700
+)
